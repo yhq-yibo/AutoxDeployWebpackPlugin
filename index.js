2705ddd834d0d36920152ef9e1aee0ed73a5da6d
aaron <chengshulun@live.com>
2022-07-13 12:07:00 +0800

    Update index.js
    比较前，统一路径分割符

--- index.js
+++ index.js
@@ -36,6 +36,8 @@ class WatchDeployPlugin {
             var modules = chunk.getModules();
             modules.forEach(module => {
             //   console.error("r---c", module.userRequest,this.changFile);
+              let userRequest = module.userRequest.replaceAll("\\", "/");
+              this.changFile = this.changFile.replaceAll("\\", "/");
               if (module.userRequest == this.changFile) {
               //  console.error("chunk", chunk.files);
                 chunk.files.forEach(file => {
